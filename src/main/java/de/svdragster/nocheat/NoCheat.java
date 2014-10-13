de69e264bbfe6b2d36b70fdadc244925383cfee5
Gordon Freeman <svdragster@gmail.com>
2014-10-13 21:34:25 +0200

    Update NoCheat.java

--- src/main/java/de/svdragster/nocheat/NoCheat.java
+++ src/main/java/de/svdragster/nocheat/NoCheat.java
@@ -4,23 +4,32 @@ import net.canarymod.Canary;
 import net.canarymod.commandsys.CommandDependencyException;
 import net.canarymod.plugin.Plugin;
 
+
 public class NoCheat extends Plugin {
 	
-	@Override
+	NoCheatListener listener = new NoCheatListener();
+	
 	public void disable() {
-		//Canary.getServer().removeSynchronousTask(new NoCheatListener().task);
+		Canary.commands().unregisterCommands(this);
 	}
 
-	@Override
 	public boolean enable() {
-		Canary.hooks().registerListener(new NoCheatListener(), this);
+		Canary.hooks().registerListener(listener, this);
 		new NoCheatListener().loadProperties();
 		try {
-			Canary.commands().registerCommands(new NoCheatCommands(), this, false);
+			Canary.commands().registerCommands(new NoCheatCommands(), this,	false);
 		} catch (CommandDependencyException e) {
 			e.printStackTrace();
 		}
+		/*if (Canary.manager().getPluginNames().contains("CustomMotd")) {
+			Plugin plugin = Canary.manager().getPlugin("CustomMotd");
+			if (plugin != null) {
+				double version = Double.parseDouble(plugin.getVersion());
+				if (version >= 0.3) {
+					listener.CustomMotdEnabled = true;
+				}
+			}
+		}*/
 		return true;
 	}
-
 }
